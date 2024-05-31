14a3d3d4240348809fcf6fa6a88acbe604e83f34
Ederson Silva <edsjcbra@gmail.com>
2024-05-31 17:26:30 +0100

    Configured API and Handlers

---
+++ FinaFlow.Core/Common/DateTimeExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace FinaFlow.Core.Common;
+
+public static class DateTimeExtension
+{
+    public static DateTime GetFirstDay(this DateTime date, int? year = null, int? month = null)
+        => new(year ?? date.Year, month ?? date.Month, day: 1);
+
+    public static DateTime GetLastDay(
+        this DateTime date, int? year = null, int? month = null)
+        => new DateTime(year ?? date.Year, month ?? date.Month, day: 1)
+            .AddMonths(1)
+            .AddDays(-1);
+}
